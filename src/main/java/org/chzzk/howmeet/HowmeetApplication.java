03382e74a68f65b22ab9f6fb17e54c0c4e4797cf
kmw2378 <rlarla677@gmail.com>
2024-07-14 13:02:20 +0900

    [feat]초기 세팅

---
+++ src/main/java/org/chzzk/howmeet/HowmeetApplication.java
@@ -0,0 +1,13 @@
+package org.chzzk.howmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/chzzk/howmeet/HowmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chzzk.howmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
