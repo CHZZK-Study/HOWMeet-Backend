4fee12c6fea04b00723ac3c2dc97f69dedea4b9d
kmw2378 <rlarla677@gmail.com>
2024-07-29 12:16:20 +0900

    [FEAT/HM-82] Guest의 Password 필드 타입 수정

--- src/main/java/org/chzzk/howmeet/domain/temporary/auth/entity/Guest.java
+++ src/main/java/org/chzzk/howmeet/domain/temporary/auth/entity/Guest.java
@@ -11,8 +11,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.chzzk.howmeet.domain.common.entity.BaseEntity;
+import org.chzzk.howmeet.domain.common.model.EncodedPassword;
 import org.chzzk.howmeet.domain.common.model.Nickname;
-import org.chzzk.howmeet.domain.common.model.Password;
+import org.chzzk.howmeet.domain.common.model.converter.EncodedPasswordConverter;
 import org.chzzk.howmeet.domain.common.model.converter.NicknameConverter;
 import org.chzzk.howmeet.domain.common.model.converter.PasswordConverter;
 import org.chzzk.howmeet.domain.temporary.schedule.entity.GuestSchedule;
@@ -31,20 +32,20 @@ public class Guest extends BaseEntity {
     private Nickname nickname;
 
     @Column(name = "password", nullable = false)
-    @Convert(converter = PasswordConverter.class)
-    private Password password;
+    @Convert(converter = EncodedPasswordConverter.class)
+    private EncodedPassword password;
 
     // todo 7/19 김민우 : 추후 cascade 를 활용하기 위해 연관관계 사용 고려
     @Column(name = "guest_schedule_id", nullable = false)
     private Long guestScheduleId;
 
-    private Guest(final Nickname nickname, final Password password, final Long guestScheduleId) {
+    private Guest(final Nickname nickname, final EncodedPassword password, final Long guestScheduleId) {
         this.nickname = nickname;
         this.password = password;
         this.guestScheduleId = guestScheduleId;
     }
 
-    public static Guest of(final GuestSchedule guestSchedule, final Nickname nickname, final Password password) {
-        return new Guest(nickname, password, guestSchedule.getId());
+    public static Guest of(final Long guestScheduleId, final String nickname, final EncodedPassword password) {
+        return new Guest(Nickname.from(nickname), password, guestScheduleId);
     }
 }
