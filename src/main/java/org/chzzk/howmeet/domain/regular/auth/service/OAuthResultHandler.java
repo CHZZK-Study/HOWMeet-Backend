165a555b90542ba51b3be017e4897903ca412cfd
kmw2378 <rlarla677@gmail.com>
2024-07-30 14:35:21 +0900

    [FEAT] OAuthResultHandler 추가 (#83) OAuthClient 결과를 통해 DB에 회원 정보를 저장하거나 가져오는 클래스 추가

---
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/service/OAuthResultHandler.java
@@ -0,0 +1,25 @@
+package org.chzzk.howmeet.domain.regular.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.domain.regular.auth.entity.Member;
+import org.chzzk.howmeet.domain.regular.auth.repository.MemberRepository;
+import org.chzzk.howmeet.infra.oauth.model.profile.OAuthProfile;
+import org.chzzk.howmeet.infra.oauth.model.profile.OAuthProfileFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class OAuthResultHandler {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Member saveOrGet(final Map<String, Object> attributes, final String providerName) {
+        final OAuthProfile oAuthProfile = OAuthProfileFactory.of(attributes, providerName);
+        return memberRepository.findBySocialId(oAuthProfile.getSocialId())
+                        .orElseGet(() -> memberRepository.save(oAuthProfile.toEntity()));
+    }
+}
