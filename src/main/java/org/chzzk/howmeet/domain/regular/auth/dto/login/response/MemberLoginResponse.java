c27bd0fce2d54d8b162c0792a5bc402a9ee275ce
kmw2378 <rlarla677@gmail.com>
2024-07-30 14:35:59 +0900

    [FEAT] MemberLoginResponse 추가 (#83)

---
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/dto/login/response/MemberLoginResponse.java
@@ -0,0 +1,9 @@
+package org.chzzk.howmeet.domain.regular.auth.dto.login.response;
+
+import org.chzzk.howmeet.domain.regular.auth.entity.Member;
+
+public record MemberLoginResponse(String accessToken, Long memberId, String nickname) {
+    public static MemberLoginResponse of(final String accessToken, final Member member) {
+        return new MemberLoginResponse(accessToken, member.getId(), member.getNickname().getValue());
+    }
+}
