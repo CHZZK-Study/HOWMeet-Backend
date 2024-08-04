ce8d6563cc131dc3748c15b6b6b48f3cabc49242
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-04 16:18:24 +0900

    [FEAT] RoomRepository 추가 (#73)

---
+++ src/main/java/org/chzzk/howmeet/domain/regular/room/controller/RoomController.java
@@ -0,0 +1,39 @@
+package org.chzzk.howmeet.domain.regular.room.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.domain.regular.schedule.dto.MemberScheduleResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RequestMapping("/room")
+@RestController
+public class RoomController {
+    private final RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<?> createRoom(@RequestBody final RoomRequest roomRequest) {
+        final RoomResponse roomResponse = roomService.createRoom(roomRequest);
+        return ResponseEntity.ok(roomResponse);
+    }
+
+    @PostMapping("/{memberRoomId}/edit")
+    public ResponseEntity<?> updateRoom(
+            @PathVariable Long roomId,
+            @RequestBody final RoomRequest roomRequest) {
+        final RoomResponse roomResponse = roomService.createMemberRoom(roomId, roomRequest);
+        return ResponseEntity.ok(roomResponse);
+    }
+
+    @GetMapping("/{memberRoomId}")
+    public ResponseEntity<?> getMemberRoom(@PathVariable Long roomId) {
+        final RoomResponse roomResponse = roomService.getRoom(roomId);
+        return ResponseEntity.ok(roomResponse);
+    }
+
+    @DeleteMapping("/{memberRoomId}")
+    public ResponseEntity<?> deleteMemberRoom(@PathVariable Long roomId) {
+        roomService.deleteRoom(roomId);
+        return ResponseEntity.noContent().build();
+    }
+}
