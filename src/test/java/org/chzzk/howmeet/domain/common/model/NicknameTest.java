e0b5caebcb119bcd9495bd460a86f04b429c4cf6
kmw2378 <rlarla677@gmail.com>
2024-08-08 11:59:02 +0900

    [TEST] NicknameTest 한글 초성, 중성, 종성 테스트 케이스 추가 (#89)

--- src/test/java/org/chzzk/howmeet/domain/common/model/NicknameTest.java
+++ src/test/java/org/chzzk/howmeet/domain/common/model/NicknameTest.java
@@ -6,11 +6,31 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.chzzk.howmeet.common.util.DisplayFormat.DISPLAY_NAME_FORMAT;
 import static org.chzzk.howmeet.domain.common.exception.NicknameErrorCode.INVALID_NICKNAME;
 
 class NicknameTest {
+    @ParameterizedTest(name = DISPLAY_NAME_FORMAT)
+    @DisplayName("알파벳(A~Z, a~z), 숫자(0~9), 언더스코어(_), 하이픈(-), 한글(가-힣, 초성, 중성, 종성) 문자 가능")
+    @CsvSource(value = {
+            "알파벳 (대문자), ABCD",
+            "알파벳 (소문자), abcd",
+            "알파벳 (대소문자), ABcd",
+            "숫자, 1234",
+            "언더스코어, h_e",
+            "하이픈, h-e",
+            "한글, 김민우",
+            "한글 (초성), ㄱㅁ민우",
+            "한글 (중성), ㅣㅏ민우",
+            "한글 (종성), ㄲㄳ민우"
+    })
+    public void validCharacters(final String displayName, final String value) throws Exception {
+        assertThatCode(() -> Nickname.from(value))
+                .doesNotThrowAnyException();
+    }
+
     @ParameterizedTest(name = DISPLAY_NAME_FORMAT)
     @DisplayName("닉네임은 2~10자만 가능")
     @CsvSource(value = {
@@ -24,7 +44,7 @@ class NicknameTest {
     }
 
     @ParameterizedTest(name = DISPLAY_NAME_FORMAT)
-    @DisplayName("알파벳(A~Z, a~z), 숫자(0~9), 언더스코어(_), 하이픈(-), 한글(가-힣) 외에 문자가 포함되면 예외 발생")
+    @DisplayName("알파벳(A~Z, a~z), 숫자(0~9), 언더스코어(_), 하이픈(-), 한글(가-힣, 초성, 중성, 종성) 외에 문자가 포함되면 예외 발생")
     @CsvSource(value = {
             "특수 문자, !hello",
             "공백 문자, hel lo",
@@ -51,7 +71,7 @@ class NicknameTest {
                 .isInstanceOf(NicknameException.class)
                 .hasMessageContaining(INVALID_NICKNAME.getMessage());
     }
-    
+
     @ParameterizedTest
     @DisplayName("비속어 필터링")
     @ValueSource(strings = { "씨발", "개새끼"})
