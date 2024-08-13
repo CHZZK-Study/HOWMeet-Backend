a03dcd24d3abd12b8d6038b532c14ee14f12a29c
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-13 17:36:31 +0900

    [REFACTOR] of -> from (#86)

--- src/main/java/org/chzzk/howmeet/domain/regular/room/dto/RoomMemberResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/room/dto/RoomMemberResponse.java
@@ -3,7 +3,7 @@ package org.chzzk.howmeet.domain.regular.room.dto;
 import org.chzzk.howmeet.domain.regular.room.entity.RoomMember;
 
 public record RoomMemberResponse(Long id, Long memberId, Boolean isLeader) {
-    public static RoomMemberResponse of(final RoomMember roomMember) {
+    public static RoomMemberResponse from(final RoomMember roomMember) {
         return new RoomMemberResponse(
                 roomMember.getId(),
                 roomMember.getMemberId(),
--- src/main/java/org/chzzk/howmeet/domain/regular/room/dto/RoomResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/room/dto/RoomResponse.java
@@ -9,7 +9,7 @@ import java.util.List;
 public record RoomResponse(Long roomId, String name, String description, List<RoomMemberResponse> roomMembers, List<MemberSchedule> memberSchedules) {
     public static RoomResponse of(final Room room, final List<RoomMember> roomMembers, final List<MemberSchedule> memberSchedules) {
         List<RoomMemberResponse> roomMemberResponse = roomMembers.stream()
-                .map(RoomMemberResponse::of)
+                .map(RoomMemberResponse::from)
                 .toList();
 
         return new RoomResponse(
