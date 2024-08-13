e8a8592ea8db2012e9f9e2989518f06a842b7cc4
kmw2378 <rlarla677@gmail.com>
2024-08-13 22:07:30 +0900

    [FEAT] OAuthClient에서 WebClient를 사용하는 메서드에 재시도, 타임 아웃 추가 (#91)

--- src/main/java/org/chzzk/howmeet/infra/oauth/service/OAuthClient.java
+++ src/main/java/org/chzzk/howmeet/infra/oauth/service/OAuthClient.java
@@ -1,10 +1,10 @@
 package org.chzzk.howmeet.infra.oauth.service;
 
-import lombok.RequiredArgsConstructor;
-import org.chzzk.howmeet.infra.oauth.util.MultiValueMapConverter;
 import org.chzzk.howmeet.infra.oauth.dto.token.request.OAuthTokenRequest;
 import org.chzzk.howmeet.infra.oauth.dto.token.response.OAuthTokenResponse;
 import org.chzzk.howmeet.infra.oauth.model.OAuthProvider;
+import org.chzzk.howmeet.infra.oauth.util.MultiValueMapConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
@@ -12,14 +12,28 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
-@RequiredArgsConstructor
 @Service
 public class OAuthClient {
     private final MultiValueMapConverter multiValueMapConverter;
     private final WebClient webClient;
+    private final int timeout;
+    private final int maxRetry;
+
+    public OAuthClient(final MultiValueMapConverter multiValueMapConverter,
+                       final WebClient webClient,
+                       @Value("${oauth.timeout}") final int timeout,
+                       @Value("${oauth.max-retry}") final int maxRetry) {
+        this.multiValueMapConverter = multiValueMapConverter;
+        this.webClient = webClient;
+        this.timeout = timeout;
+        this.maxRetry = maxRetry;
+    }
 
     public Mono<Map<String, Object>> getProfile(final OAuthProvider provider, final String code) {
         return issueToken(provider, code)
@@ -32,8 +46,11 @@ public class OAuthClient {
                 .headers(header -> header.setBearerAuth(socialAccessToken))
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<>() {
-                });
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
+                })
+                .timeout(Duration.ofMillis(timeout))
+                .retryWhen(Retry.max(maxRetry).filter(this::isRetryable))
+                .onErrorMap(TimeoutException.class, e -> new IllegalStateException("요청 시간이 초과되었습니다.", e));
     }
 
     private Mono<OAuthTokenResponse> issueToken(final OAuthProvider provider, final String code) {
@@ -42,10 +59,17 @@ public class OAuthClient {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(BodyInserters.fromFormData(getIssueTokenParams(provider, code)))
                 .retrieve()
-                .bodyToMono(OAuthTokenResponse.class);
+                .bodyToMono(OAuthTokenResponse.class)
+                .timeout(Duration.ofMillis(timeout))
+                .retryWhen(Retry.max(maxRetry).filter(this::isRetryable))
+                .onErrorMap(TimeoutException.class, e -> new IllegalStateException("요청 시간이 초과되었습니다.", e));
     }
 
     private MultiValueMap<String, String> getIssueTokenParams(final OAuthProvider provider, final String code) {
         return multiValueMapConverter.convertFrom(OAuthTokenRequest.of(provider, code));
+    }
+
+    private boolean isRetryable(Throwable ex) {
+        return (ex instanceof IllegalStateException) || (ex instanceof TimeoutException);
     }
 }
