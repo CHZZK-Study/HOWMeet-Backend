8308df4eec5d652b0c104a5b4f4ab13c28eee8d2
chech2 <chaelim3396@gmail.com>
2024-08-18 07:54:02 +0900

    [FEAT] GSRecordController 추가 (#75)

---
+++ src/main/java/org/chzzk/howmeet/domain/temporary/record/controller/GSRecordController.java
@@ -0,0 +1,36 @@
+package org.chzzk.howmeet.domain.temporary.record.controller;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.domain.common.auth.annotation.Authenticated;
+import org.chzzk.howmeet.domain.temporary.auth.entity.Guest;
+import org.chzzk.howmeet.domain.temporary.record.dto.post.request.GSRecordPostRequest;
+import org.chzzk.howmeet.domain.temporary.record.service.GSRecordService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/gs-record")
+@RestController
+public class GSRecordController {
+    final GSRecordService gsRecordService;
+
+    @GetMapping("/{gsId}")
+    public ResponseEntity<?> getGSRecord(@PathVariable(value = "gsId") Long gsId){
+
+        return ResponseEntity.ok(gsRecordService.getGSRecord(gsId));
+    }
+
+
+    @PostMapping("/post")
+    public ResponseEntity<?> postGSRecord(@RequestBody final GSRecordPostRequest gsRecordPostRequest, @Authenticated Guest guest){
+        gsRecordService.postGSRecord(gsRecordPostRequest,  guest);
+        return ResponseEntity.ok(CREATED);
+    }
+}
