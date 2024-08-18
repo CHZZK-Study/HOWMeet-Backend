22c9256a419431deafdff87d280149baf858809d
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-18 22:13:32 +0900

    [FEAT] MSException 추가 (#92)

--- src/main/java/org/chzzk/howmeet/domain/temporary/schedule/exception/GSException.java
+++ src/main/java/org/chzzk/howmeet/domain/temporary/schedule/exception/GSException.java
@@ -1,10 +1,10 @@
 package org.chzzk.howmeet.domain.temporary.schedule.exception;
 
 public class GSException extends RuntimeException {
-    private final GSErrorCode gsErrorCode;
+    private final GSErrorCode errorCode;
 
-    public GSException(final GSErrorCode gsErrorCode) {
-        super(gsErrorCode.getMessage());
-        this.gsErrorCode = gsErrorCode;
+    public GSException(final GSErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
     }
 }
