f879dca13d67087373ffde02d63a49e855856c09
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-18 22:15:23 +0900

    [FEAT] MSService 예외 처리 추가 (#92)

--- src/main/java/org/chzzk/howmeet/domain/regular/schedule/service/MSService.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/schedule/service/MSService.java
@@ -1,15 +1,18 @@
 package org.chzzk.howmeet.domain.regular.schedule.service;
 
 import lombok.RequiredArgsConstructor;
-import org.chzzk.howmeet.domain.common.util.InviteUrlProvider;
 import org.chzzk.howmeet.domain.regular.room.entity.Room;
 import org.chzzk.howmeet.domain.regular.room.repository.RoomRepository;
 import org.chzzk.howmeet.domain.regular.schedule.dto.MSRequest;
 import org.chzzk.howmeet.domain.regular.schedule.dto.MSResponse;
 import org.chzzk.howmeet.domain.regular.schedule.entity.MemberSchedule;
+import org.chzzk.howmeet.domain.regular.schedule.exception.MSException;
 import org.chzzk.howmeet.domain.regular.schedule.repository.MSRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.chzzk.howmeet.domain.regular.schedule.exception.MSErrorCode.ROOM_NOT_FOUND;
+import static org.chzzk.howmeet.domain.regular.schedule.exception.MSErrorCode.SCHEDULE_NOT_FOUND;
 
 @RequiredArgsConstructor
 @Service
@@ -17,35 +20,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class MSService {
     private final MSRepository msRepository;
     private final RoomRepository roomRepository;
-    private final InviteUrlProvider inviteUrlProvider;
+    //private final InviteUrlProvider inviteUrlProvider;
 
     @Transactional
     public MSResponse createMemberSchedule(final MSRequest msRequest) {
         Room room = roomRepository.findById(msRequest.roomId())
-                .orElseThrow(() -> new IllegalArgumentException("Invalid room ID"));
+                .orElseThrow(() -> new MSException(ROOM_NOT_FOUND));
 
         MemberSchedule memberSchedule = MemberSchedule.of(msRequest.dates(), msRequest.time(), msRequest.name(), room);
 
         MemberSchedule savedSchedule = msRepository.save(memberSchedule);
 
-        String inviteLink = inviteUrlProvider.generateInviteUrl("member-schedule", savedSchedule.getId());
+        // String inviteLink = inviteUrlProvider.generateInviteUrl("member-schedule", savedSchedule.getId());
 
-        return MSResponse.of(savedSchedule, inviteLink);
+        return MSResponse.of(savedSchedule);
     }
 
     public MSResponse getMemberSchedule(final Long memberScheduleId) {
         MemberSchedule memberSchedule = msRepository.findById(memberScheduleId)
-                .orElseThrow(() -> new IllegalArgumentException("Invalid schedule ID"));
+                .orElseThrow(() -> new MSException(SCHEDULE_NOT_FOUND));
 
-        String inviteLink = inviteUrlProvider.generateInviteUrl("member-schedule", memberScheduleId);
+        // String inviteLink = inviteUrlProvider.generateInviteUrl("member-schedule", memberScheduleId);
 
-        return MSResponse.of(memberSchedule, inviteLink);
+        return MSResponse.of(memberSchedule);
     }
 
     @Transactional
     public void deleteMemberSchedule(final Long memberScheduleId) {
         if (!msRepository.existsById(memberScheduleId)) {
-            throw new IllegalArgumentException("Invalid schedule ID");
+            throw new MSException(SCHEDULE_NOT_FOUND);
         }
         msRepository.deleteById(memberScheduleId);
     }
