a22f62ce6c934225821f4d8852c920efcfd99a1d
SSUHYUNKIM <yellosuhyun@naver.com>
2024-08-18 21:55:03 +0900

    [FEAT] MSErrorCode 추가 (#92)

--- src/main/java/org/chzzk/howmeet/domain/regular/schedule/exception/MSErrorCode.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/schedule/exception/MSErrorCode.java
@@ -1,17 +1,18 @@
-package org.chzzk.howmeet.domain.temporary.schedule.exception;
+package org.chzzk.howmeet.domain.regular.schedule.exception;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum GSErrorCode {
+public enum MSErrorCode {
     SCHEDULE_NOT_FOUND("스케줄을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    ROOM_NOT_FOUND("룸을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     SCHEDULE_CREATION_FAILED("스케줄 생성에 실패하였습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String message;
     private final HttpStatus status;
 
-    GSErrorCode(final String message, final HttpStatus status) {
+    MSErrorCode(final String message, final HttpStatus status) {
         this.message = message;
         this.status = status;
     }
