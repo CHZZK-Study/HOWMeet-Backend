a463536c14ffb824f3257583914394871c8bec3e
kmw2378 <rlarla677@gmail.com>
2024-08-23 15:12:45 +0900

    [FEAT] MemberRepository.findSummaryById() 추가 (#106)

--- src/main/java/org/chzzk/howmeet/domain/regular/member/repository/MemberRepository.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/member/repository/MemberRepository.java
@@ -1,10 +1,18 @@
 package org.chzzk.howmeet.domain.regular.member.repository;
 
+import org.chzzk.howmeet.domain.regular.member.dto.summary.dto.MemberSummaryDto;
 import org.chzzk.howmeet.domain.regular.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findBySocialId(final String socialId);
+
+    @Query("SELECT NEW org.chzzk.howmeet.domain.regular.member.dto.summary.dto.MemberSummaryDto(m.id, m.nickname) " +
+            "FROM Member m " +
+            "WHERE m.id =:id")
+    Optional<MemberSummaryDto> findSummaryById(@Param("id") final Long id);
 }
