e3048758ad4a73cd28579e7b8f183ea4057e4d52
chech2 <chaelim3396@gmail.com>
2024-08-25 18:09:46 +0900

    [REFACTOR] findByGuestScheduleId메소드 param 수정 (#75)

--- src/main/java/org/chzzk/howmeet/domain/temporary/record/repository/TmpGuestRepository.java
+++ src/main/java/org/chzzk/howmeet/domain/temporary/record/repository/TmpGuestRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface TmpGuestRepository extends JpaRepository<Guest, Long> {
-    List<Guest> findByGuestScheduleId(@Param("guest_schedule_id") final Long guestScheduleId);
+    List<Guest> findByGuestScheduleId(@Param("guestScheduleId") final Long guestScheduleId);
 
 }
