e30854a9c93d5b79553927e430f87ff167fb2e04
chech2 <chaelim3396@gmail.com>
2024-08-25 19:27:56 +0900

    [REFACTOR] 임시로 사용한 repository 제거 (#75) - develop 브랜치와 머지 전 임시로 사용한 repository 제거: TmpRoomMemberRepository, TmpMSRepository, TmpMemberRepository, TmpGSRepository

--- src/main/java/org/chzzk/howmeet/domain/regular/record/repository/TmpMSRepository.java
+++
@@ -1,8 +0,0 @@
-package org.chzzk.howmeet.domain.regular.record.repository;
-
-import org.chzzk.howmeet.domain.regular.schedule.entity.MemberSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TmpMSRepository extends JpaRepository<MemberSchedule, Long> {
-
-}
--- src/main/java/org/chzzk/howmeet/domain/temporary/record/repository/TmpGSRepository.java
+++
@@ -1,7 +0,0 @@
-package org.chzzk.howmeet.domain.temporary.record.repository;
-
-import org.chzzk.howmeet.domain.temporary.schedule.entity.GuestSchedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TmpGSRepository extends JpaRepository<GuestSchedule, Long> {
-}
