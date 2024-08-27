522798c9ff7356445468c23e6511430463e8622c
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-08-27 16:27:57 +0900

    Revert "[머지 브랜치] develop 브랜치 유실된 내용 복구 (#22)" (#25)

--- src/main/java/org/chzzk/howmeet/infra/oauth/exception/UnsupportedProviderException.java
+++
@@ -1,7 +0,0 @@
-package org.chzzk.howmeet.infra.oauth.exception;
-
-public class UnsupportedProviderException extends RuntimeException {
-    public UnsupportedProviderException(final String providerName) {
-        super(providerName + " 소셜 로그인은 지원하지 않습니다.");
-    }
-}
