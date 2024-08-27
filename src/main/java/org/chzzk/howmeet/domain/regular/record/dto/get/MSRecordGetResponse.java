17adca898e05f32a98dab49f41595ac35b5290df
chech2 <chaelim3396@gmail.com>
2024-08-27 15:42:37 +0900

    [REFACTOR] RoomName 추가 (#75)

--- src/main/java/org/chzzk/howmeet/domain/regular/record/dto/get/MSRecordGetResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/record/dto/get/MSRecordGetResponse.java
@@ -3,12 +3,13 @@ package org.chzzk.howmeet.domain.regular.record.dto.get;
 import java.util.List;
 import org.chzzk.howmeet.domain.common.model.NicknameList;
 import org.chzzk.howmeet.domain.common.model.SelectionDetail;
+import org.chzzk.howmeet.domain.regular.room.model.RoomName;
 
-public record MSRecordGetResponse(Long msId, NicknameList totalPersonnel, NicknameList participatedPersonnel,
-                                  List<SelectionDetail> selectTime) {
+public record MSRecordGetResponse(Long msId, RoomName roomName, NicknameList totalPersonnel,
+                                  NicknameList participatedPersonnel, List<SelectionDetail> selectTime) {
 
-    public static MSRecordGetResponse of(final Long msId, final NicknameList totalPersonnel,
+    public static MSRecordGetResponse of(final Long msId, final RoomName roomName, final NicknameList totalPersonnel,
             final NicknameList participatedPersonnel, final List<SelectionDetail> selectTime) {
-        return new MSRecordGetResponse(msId, totalPersonnel, participatedPersonnel, selectTime);
+        return new MSRecordGetResponse(msId, roomName, totalPersonnel, participatedPersonnel, selectTime);
     }
 }
