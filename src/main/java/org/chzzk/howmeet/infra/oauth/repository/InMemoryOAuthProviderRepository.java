e3ab1beb8516e11ff3c1708e54ea99c5ad8d3a85
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-08-27 16:35:58 +0900

    [기능 추가] 회원 기능 예외 처리 (#29)

--- src/main/java/org/chzzk/howmeet/infra/oauth/repository/InMemoryOAuthProviderRepository.java
+++ src/main/java/org/chzzk/howmeet/infra/oauth/repository/InMemoryOAuthProviderRepository.java
@@ -1,6 +1,7 @@
 package org.chzzk.howmeet.infra.oauth.repository;
 
 import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.infra.oauth.exception.UnsupportedProviderException;
 import org.chzzk.howmeet.infra.oauth.model.OAuthProvider;
 
 import java.util.Map;
@@ -10,6 +11,9 @@ public class InMemoryOAuthProviderRepository {
     private final Map<String, OAuthProvider> providers;
 
     public OAuthProvider findByProviderName(String providerName) {
+        if (!providers.containsKey(providerName)) {
+            throw new UnsupportedProviderException(providerName);
+        }
         return providers.get(providerName);
     }
 }
---
+++ src/main/java/org/chzzk/howmeet/infra/oauth/service/OAuthTimeoutDecorator.java
@@ -0,0 +1,31 @@
+package org.chzzk.howmeet.infra.oauth.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
+
+@Component
+public class OAuthTimeoutDecorator {
+    private final int timeout;
+    private final int maxRetry;
+
+    public OAuthTimeoutDecorator(@Value("${oauth.timeout}") final int timeout,
+                                 @Value("${oauth.max-retry}") final int maxRetry) {
+        this.timeout = timeout;
+        this.maxRetry = maxRetry;
+    }
+
+    public <T> Mono<T> decorate(final Mono<T> mono) {
+        return mono.timeout(Duration.ofMillis(timeout))
+                .retryWhen(Retry.max(maxRetry).filter(this::isRetryable))
+                .onErrorMap(TimeoutException.class, e -> new IllegalStateException("요청 시간이 초과되었습니다.", e));
+    }
+
+    private boolean isRetryable(Throwable ex) {
+        return (ex instanceof IllegalStateException) || (ex instanceof TimeoutException);
+    }
+}
