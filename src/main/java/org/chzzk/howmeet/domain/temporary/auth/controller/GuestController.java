12265beca2e3ec6747e4e1d2b8d3a5c7e52a20bf
kmw2378 <rlarla677@gmail.com>
2024-09-01 13:15:20 +0900

    [REFACTOR] GuestController.signup() 삭제 (#122)

--- src/main/java/org/chzzk/howmeet/domain/temporary/auth/controller/GuestController.java
+++ src/main/java/org/chzzk/howmeet/domain/temporary/auth/controller/GuestController.java
@@ -3,16 +3,12 @@ package org.chzzk.howmeet.domain.temporary.auth.controller;
 import lombok.RequiredArgsConstructor;
 import org.chzzk.howmeet.domain.temporary.auth.dto.login.request.GuestLoginRequest;
 import org.chzzk.howmeet.domain.temporary.auth.dto.login.response.GuestLoginResponse;
-import org.chzzk.howmeet.domain.temporary.auth.dto.signup.request.GuestSignupRequest;
-import org.chzzk.howmeet.domain.temporary.auth.dto.signup.response.GuestSignupResponse;
 import org.chzzk.howmeet.domain.temporary.auth.service.GuestService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.HttpStatus.CREATED;
 
 @RequiredArgsConstructor
 @RequestMapping("/auth")
@@ -24,12 +20,5 @@ public class GuestController {
     public ResponseEntity<?> login(@RequestBody final GuestLoginRequest guestLoginRequest) {
         final GuestLoginResponse guestLoginResponse = guestService.login(guestLoginRequest);
         return ResponseEntity.ok(guestLoginResponse);
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<?> signup(@RequestBody final GuestSignupRequest guestSignupRequest) {
-        final GuestSignupResponse guestSignupResponse = guestService.signup(guestSignupRequest);
-        return ResponseEntity.status(CREATED)
-                .body(guestSignupResponse);
     }
 }
