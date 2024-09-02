d070ae6d1b00950544ac7b006c7b913893dd7fb8
chech2 <chaelim3396@gmail.com>
2024-09-03 03:17:34 +0900

    [REFACTOR] Nicknames getter 추가 (#100) - getter 없을 경우, 직렬화가 되지 않아 에러 발생

--- src/main/java/org/chzzk/howmeet/domain/common/model/Nicknames.java
+++ src/main/java/org/chzzk/howmeet/domain/common/model/Nicknames.java
@@ -33,6 +33,10 @@ public class Nicknames {
         this.nicknames.addAll(nickNameSet);
     }
 
+    public List<Nickname> getNicknames() {
+        return List.copyOf(nicknames);
+    }
+
     public static Nicknames convertNicknameProvidersList(final List<? extends NicknameProvider> nicknameProviders) {
         Nicknames nicknames = Nicknames.create();
 
