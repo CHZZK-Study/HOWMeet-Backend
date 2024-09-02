fddf319c890bc0bf1346fbfc525ca07af2d1616d
Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
2024-09-03 00:01:18 +0900

    [기능 추가] 인증/인가 예외 처리 (#41)
    * [HOTFIX] HowmeetApplicationTests 어노테이션에 속성 추가
    
    * [CHORE] copySwaggerUI 의존성 수정
    
    * [REFACTOR] InviteUrlProvider 주석 처리
    
    * [HOTFIX] build.gradle bootJar에 닫는 중괄호 추가
    
    * [FEAT] AuthException에 status 필드 추가  (#128)
    
    * [REFACTOR] AuthErrorCode 에 status 필드 제거  (#128)
    
    * [FEAT] AuthorizationException 추가 (#128)
    
    * [FEAT] AuthenticationException 추가 (#128)
    
    * [REFACTOR] 인증/인가 인터셉터 예외 클래스 수정 (#128)
    
    * [FEAT] 인가 인터셉터에서 토큰이 비어있는 경우 예외 처리 (#128)
    
    * [FEAT] WebConfig.addInterceptors() 에 MemberAuthorityInterceptor 추가 (#128)
    
    * [FEAT] GlobalExceptionHandler 에 handleAuthException() 메서드 추가 (#128)
    
    * [REFACTOR] GlobalExceptionHandler.handleBusinessException() 메서드명 수정 (#128)

--- src/test/java/org/chzzk/howmeet/HowmeetApplicationTests.java
+++ src/test/java/org/chzzk/howmeet/HowmeetApplicationTests.java
@@ -3,7 +3,7 @@ package org.chzzk.howmeet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HowmeetApplicationTests.class)
 class HowmeetApplicationTests {
 
 	@Test
