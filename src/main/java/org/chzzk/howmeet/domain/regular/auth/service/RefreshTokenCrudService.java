99e83f6f13382a027a363f6499bc10f78d67d24f
kmw2378 <rlarla677@gmail.com>
2024-09-02 22:01:13 +0900

    [FEAT] RefreshTokenCrudService 추가 (#130)

---
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/service/RefreshTokenCrudService.java
@@ -0,0 +1,25 @@
+package org.chzzk.howmeet.domain.regular.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.domain.common.auth.model.AuthPrincipal;
+import org.chzzk.howmeet.domain.regular.auth.entity.RefreshToken;
+import org.chzzk.howmeet.domain.regular.auth.repository.RefreshTokenRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class RefreshTokenCrudService {
+    private final RefreshTokenProvider refreshTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public RefreshToken save(final AuthPrincipal authPrincipal) {
+        return refreshTokenRepository.save(refreshTokenProvider.createToken(authPrincipal));
+    }
+
+    public void delete(final AuthPrincipal authPrincipal, final String value) {
+        refreshTokenRepository.deleteByMemberIdAndValue(authPrincipal.id(), value);
+    }
+}
