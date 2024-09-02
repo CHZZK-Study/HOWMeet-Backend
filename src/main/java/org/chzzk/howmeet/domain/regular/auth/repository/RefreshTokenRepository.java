973a1c8d73ab58d17200dc0deb79d80b4cb1d98a
kmw2378 <rlarla677@gmail.com>
2024-09-03 01:17:14 +0900

    [FEAT] RefreshTokenRepository.findByValue() 추가 (#130)

--- src/main/java/org/chzzk/howmeet/domain/regular/auth/repository/RefreshTokenRepository.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/repository/RefreshTokenRepository.java
@@ -2,8 +2,9 @@ package org.chzzk.howmeet.domain.regular.auth.repository;
 
 import org.chzzk.howmeet.domain.regular.auth.entity.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
-    void deleteByMemberIdAndValue(@Param("memberId") final Long memberId, @Param("value") final String value);
+    Optional<RefreshToken> findByValue(final String value);
 }
