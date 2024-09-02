48a493e766a515390b9c6aca4deed43f6389fd41
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-03 00:21:58 +0900

    [기능 추가] 방/목록 상세 조회 (#35)
    * [REFACTOR] build.gradle 누락 수정 (#111)
    
    * [REFACTOR] RoomResponse 수정 (#111)
    
    * [FEAT] RoomController getJoinedRooms 추가 (#111)
    
    * [FEAT] RoomListResponse 추가 (#111)
    
    * [FEAT] RoomService getJoinedRooms 추가 (#111)
    
    * [FEAT] RoomService getJoinedRooms 추가 (#111)
    
    * [FEAT] RoomMemberRepository findByMemberId 추가 (#111)
    
    * [DOCS] RoomController Test + Api 문서화 (#111)
    
    * Squashed commit of the following:
    
    commit 14d5fdb0e168596556b0e4c911b8baceb1a9e463
    Author: Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
    Date:   Sat Aug 31 23:42:11 2024 +0900
    
        [기능 추가] 회원/비회원 기능 요청 파라미터 검증 (#33)
    
        * [FEAT] GuestController 메서드 파라미터에 @Valid 추가 (#120)
    
        * [FEAT] RegularAuthController 메서드 파라미터에 @Valid 추가 (#120)
    
        * [FEAT] GuestLoginRequest 필드에 검증 어노테이션 추가 (#120)
    
        * [FEAT] GuestSignupRequest 필드에 검증 어노테이션 추가 (#120)
    
        * [FEAT] MemberLoginRequest 필드에 검증 어노테이션 추가 (#120)
    
    commit 80aac3b30f59329e76735abeefce7047ffd28251
    Author: Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
    Date:   Sat Aug 31 23:41:59 2024 +0900
    
        [기능 추가] 전역 예외 처리 (#32)
    
        * [FEAT] DomainException 추가 (#96)
    
        * [FEAT] DomainErrorCode 추가 (#96)
    
        * [FEAT] ErrorResponse 추가 (#96)
    
        * [FEAT] GlobalExceptionHandler 추가 (#96)
    
        * [FEAT] InfraException 추가 (#96)
    
        * [FEAT] OAuthClientException 추가 (#96)
    
        * [FEAT] OAuthServerException 추가 (#96)
    
        * [REFACTOR] OAuth 관련 예외 클래스에 OAuthClientException, OAuthServerException 적용 (#96)
    
        * [FEAT] GlobalExceptionHandler에 InfraException 추가 (#96)
    
        * [REFACTOR] 도메인 예외 클래스, 에러 코드 전역 처리를 위해 상속, 구현 관계 추가 (#96)
    
    * [REFACTOR] 필드명 수정 (#111)
    
    * [REFACTOR] unknown -> null (#111)
    
    * [REFACTOR] RoomListMapper 추가 (#111)
    
    * [REFACTOR] status 컬럼을 추가해서 하나의 리스트로 제공 (#111)
    
    * [REFACTOR] id와 닉네임만 가져오는 MemberNicknameDto 생성 (#111)
    
    * [REFACTOR] map() 내부 로직을 메서드로 추출(#118)

--- src/main/java/org/chzzk/howmeet/domain/regular/room/dto/RoomResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/room/dto/RoomResponse.java
@@ -2,14 +2,25 @@ package org.chzzk.howmeet.domain.regular.room.dto;
 
 import org.chzzk.howmeet.domain.regular.room.entity.Room;
 import org.chzzk.howmeet.domain.regular.room.entity.RoomMember;
+import org.chzzk.howmeet.domain.regular.schedule.dto.MSResponse;
 import org.chzzk.howmeet.domain.regular.schedule.entity.MemberSchedule;
+import org.chzzk.howmeet.domain.regular.schedule.entity.ScheduleStatus;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-public record RoomResponse(Long roomId, String name, String description, List<RoomMemberResponse> roomMembers, List<MemberSchedule> memberSchedules) {
+public record RoomResponse(Long roomId, String name, String description,
+                           List<RoomMemberResponse> roomMembers,
+                           List<MSResponse> schedules) {
+
     public static RoomResponse of(final Room room, final List<RoomMember> roomMembers, final List<MemberSchedule> memberSchedules) {
         List<RoomMemberResponse> roomMemberResponse = roomMembers.stream()
                 .map(RoomMemberResponse::from)
+                .collect(Collectors.toList());
+
+        List<MSResponse> schedules = memberSchedules.stream()
+                .sorted((a, b) -> a.getCreatedAt().compareTo(b.getCreatedAt())) // 생성 순서대로 정렬
+                .map(MSResponse::from)
                 .toList();
 
         return new RoomResponse(
@@ -17,7 +28,7 @@ public record RoomResponse(Long roomId, String name, String description, List<Ro
                 room.getName().getValue(),
                 room.getDescription().getValue(),
                 roomMemberResponse,
-                memberSchedules
+                schedules
         );
     }
 }
