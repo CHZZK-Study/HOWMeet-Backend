0031859862adae9f58fa2219f39ceefd0aa1a578
kmw2378 <rlarla677@gmail.com>
2024-09-02 22:01:00 +0900

    [FEAT] RefreshTokenProvider 추가 (#130)

---
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/service/RefreshTokenProvider.java
@@ -0,0 +1,22 @@
+package org.chzzk.howmeet.domain.regular.auth.service;
+
+import org.chzzk.howmeet.domain.common.auth.model.AuthPrincipal;
+import org.chzzk.howmeet.domain.regular.auth.entity.RefreshToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class RefreshTokenProvider {
+    private final Long expiration;
+
+    public RefreshTokenProvider(@Value("${auth.refresh-token.expiration}") final Long expiration) {
+        this.expiration = expiration;
+    }
+
+    public RefreshToken createToken(final AuthPrincipal authPrincipal) {
+        final String value = UUID.randomUUID().toString();
+        return RefreshToken.of(authPrincipal, value, expiration);
+    }
+}
