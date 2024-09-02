a7aafee1b7ff482e46a6e6c5ddfe81a731b731c5
kmw2378 <rlarla677@gmail.com>
2024-09-03 01:48:19 +0900

    [FEAT] RefreshTokenErrorCode에 REFRESH_TOKEN_NOT_MATCHED 열거 상수 추가 (#130)

--- src/main/java/org/chzzk/howmeet/domain/regular/auth/exception/RefreshTokenErrorCode.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/exception/RefreshTokenErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum RefreshTokenErrorCode implements DomainErrorCode {
     REFRESH_TOKEN_NO_AUTHORITY("정기 회원이 아닙니다.", HttpStatus.BAD_REQUEST),
-    REFRESH_TOKEN_NOT_FOUND("리프레시 토큰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    REFRESH_TOKEN_NOT_FOUND("리프레시 토큰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    REFRESH_TOKEN_NOT_MATCHED("요청 헤더의 리프레시 토큰값에 해당하는 회원이 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
