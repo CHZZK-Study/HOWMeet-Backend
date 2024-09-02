b6d39f60cacf1e138227da83c65e81803a7bd344
kmw2378 <rlarla677@gmail.com>
2024-09-02 14:25:31 +0900

    [REFACTOR] NicknameException이 DomainException을 상속받도록 수정 (#122)

--- src/main/java/org/chzzk/howmeet/domain/common/exception/NicknameErrorCode.java
+++ src/main/java/org/chzzk/howmeet/domain/common/exception/NicknameErrorCode.java
@@ -1,10 +1,11 @@
 package org.chzzk.howmeet.domain.common.exception;
 
 import lombok.Getter;
+import org.chzzk.howmeet.domain.common.error.DomainErrorCode;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum NicknameErrorCode {
+public enum NicknameErrorCode implements DomainErrorCode {
     INVALID_NICKNAME("닉네임이 올바르지 않습니다.", HttpStatus.BAD_REQUEST);
 
     private final String message;
