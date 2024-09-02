4d9cbcf548fb474e5856ec5b62bd29d365b5e637
chech2 <chaelim3396@gmail.com>
2024-09-03 03:31:48 +0900

    [REFACTOR] MSRecordServiceTest 추가 (#100) - 잘못된 일정의 MSRecord 날짜 입력 - 잘못된 일정의 MSRecord 시간 입력 - 잘못된 룸멤버의 post 요청 - 존재하지 않는 MSId에 의한 post 요청

--- src/test/java/org/chzzk/howmeet/domain/regular/record/service/MSRecordServiceTest.java
+++ src/test/java/org/chzzk/howmeet/domain/regular/record/service/MSRecordServiceTest.java
@@ -1,5 +1,9 @@
 package org.chzzk.howmeet.domain.regular.record.service;
 
+import static org.chzzk.howmeet.domain.regular.record.exception.MSRecordErrorCode.DATE_INVALID_SELECT;
+import static org.chzzk.howmeet.domain.regular.record.exception.MSRecordErrorCode.TIME_INVALID_SELECT;
+import static org.chzzk.howmeet.domain.regular.room.exception.RoomErrorCode.ROOM_MEMBER_NOT_FOUND;
+import static org.chzzk.howmeet.domain.regular.schedule.exception.MSErrorCode.SCHEDULE_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyList;
@@ -20,12 +24,15 @@ import org.chzzk.howmeet.domain.regular.member.repository.MemberRepository;
 import org.chzzk.howmeet.domain.regular.record.dto.get.MSRecordGetResponse;
 import org.chzzk.howmeet.domain.regular.record.dto.post.MSRecordPostRequest;
 import org.chzzk.howmeet.domain.regular.record.entity.MemberScheduleRecord;
+import org.chzzk.howmeet.domain.regular.record.exception.MSRecordException;
 import org.chzzk.howmeet.domain.regular.record.repository.MSRecordRepository;
 import org.chzzk.howmeet.domain.regular.room.entity.Room;
 import org.chzzk.howmeet.domain.regular.room.entity.RoomMember;
+import org.chzzk.howmeet.domain.regular.room.exception.RoomException;
 import org.chzzk.howmeet.domain.regular.room.repository.RoomMemberRepository;
 import org.chzzk.howmeet.domain.regular.room.repository.RoomRepository;
 import org.chzzk.howmeet.domain.regular.schedule.entity.MemberSchedule;
+import org.chzzk.howmeet.domain.regular.schedule.exception.MSException;
 import org.chzzk.howmeet.domain.regular.schedule.repository.MSRepository;
 import org.chzzk.howmeet.fixture.MSFixture;
 import org.chzzk.howmeet.fixture.MemberFixture;
@@ -65,24 +72,20 @@ public class MSRecordServiceTest {
                 LocalDateTime.of(2023, 1, 1, 11, 30)
         );
 
-        MSRecordPostRequest msRecordPostRequest = new MSRecordPostRequest(memberSchedule.getId(), selectTimes);
+        MSRecordPostRequest msRecordPostRequest = new MSRecordPostRequest(1L, selectTimes);
         AuthPrincipal authPrincipal = new AuthPrincipal(1L, member.getNickname().getValue(), member.getRole());
 
         when(msRepository.findById(memberSchedule.getId())).thenReturn(Optional.of(memberSchedule));
-        when(memberRepository.findById(anyLong())).thenAnswer(invocation -> {
-            Long id = invocation.getArgument(0);
-            return Optional.of(Member.of("김민우", "Kim", "123"));
-        });
 
         msRecordService.postMSRecord(msRecordPostRequest, authPrincipal);
 
-        verify(msRecordRepository).deleteByMemberScheduleIdAndMemberId(memberSchedule.getId(), member.getId());
+        verify(msRecordRepository).deleteByMemberScheduleIdAndMemberId(1L, 1L);
         verify(msRecordRepository).saveAll(anyList());
     }
 
     @Test
-    @DisplayName("잘못된 일정의 MemberScheduleRecord 입력으로 예외 발생")
-    void testPostMSRecord_InvalidDate_ThrowsException() {
+    @DisplayName("잘못된 일정의 MemberScheduleRecord의 날짜 입력으로 예외 발생")
+    void postMSRecord_InvalidDate() {
         Member member = MemberFixture.KIM.생성();
         MemberSchedule memberSchedule = MSFixture.createMemberScheduleA(RoomFixture.createRoomA());
         List<LocalDateTime> selectTimes = Arrays.asList(
@@ -95,16 +98,81 @@ public class MSRecordServiceTest {
         AuthPrincipal authPrincipal = new AuthPrincipal(1L, member.getNickname().getValue(), member.getRole());
 
         when(msRepository.findById(memberSchedule.getId())).thenReturn(Optional.of(memberSchedule));
-        when(memberRepository.findById(anyLong())).thenAnswer(invocation -> {
-            Long id = invocation.getArgument(0);
-            return Optional.of(Member.of("김민우", "Kim", "123"));
-        });
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+        MSRecordException exception = assertThrows(MSRecordException.class, () -> {
             msRecordService.postMSRecord(msRecordPostRequest, authPrincipal);
         });
 
-        assertEquals("선택할 수 없는 날짜를 선택하셨습니다.", exception.getMessage());
+        assertEquals(DATE_INVALID_SELECT.getMessage(), exception.getMessage());
+        assertEquals(DATE_INVALID_SELECT.getStatus(), exception.getStatus());
+    }
+
+    @Test
+    @DisplayName("잘못된 일정의 MemberScheduleRecord의 시간 입력으로 예외 발생")
+    void postMSRecord_InvalidTime() {
+        Member member = MemberFixture.KIM.생성();
+        MemberSchedule memberSchedule = MSFixture.createMemberScheduleA(RoomFixture.createRoomA());
+        List<LocalDateTime> selectTimes = Arrays.asList(
+                LocalDateTime.of(2023, 1, 1, 8, 30)
+//                LocalDateTime.of(2023, 1, 1, 11, 0),
+//                LocalDateTime.of(2020, 1, 1, 11, 30)
+        );
+
+        MSRecordPostRequest msRecordPostRequest = new MSRecordPostRequest(memberSchedule.getId(), selectTimes);
+        AuthPrincipal authPrincipal = new AuthPrincipal(1L, member.getNickname().getValue(), member.getRole());
+
+        when(msRepository.findById(memberSchedule.getId())).thenReturn(Optional.of(memberSchedule));
+
+        MSRecordException exception = assertThrows(MSRecordException.class, () -> {
+            msRecordService.postMSRecord(msRecordPostRequest, authPrincipal);
+        });
+
+        assertEquals(TIME_INVALID_SELECT.getMessage(), exception.getMessage());
+        assertEquals(TIME_INVALID_SELECT.getStatus(), exception.getStatus());
+    }
+
+    @Test
+    @DisplayName("room에 존재하지 않는 멤버로 post요청 시 예외 발생")
+    public void postMSRecord_InvaildRoomMember(){
+        Member member = MemberFixture.KIM.생성();
+        MemberSchedule memberSchedule = MSFixture.createMemberScheduleA(RoomFixture.createRoomA());
+        List<LocalDateTime> selectTimes = Arrays.asList(
+                LocalDateTime.of(2023, 1, 1, 9, 30)
+        );
+        MSRecordPostRequest msRecordPostRequest = new MSRecordPostRequest(memberSchedule.getId(), selectTimes);
+        AuthPrincipal authPrincipal = new AuthPrincipal(2345L, member.getNickname().getValue(), member.getRole());
+
+        when(msRepository.findById(memberSchedule.getId())).thenReturn(Optional.of(memberSchedule));
+
+        RoomException exception = assertThrows(RoomException.class, () -> {
+            msRecordService.postMSRecord(msRecordPostRequest, authPrincipal);
+        });
+
+        assertEquals(ROOM_MEMBER_NOT_FOUND.getMessage(), exception.getMessage());
+        assertEquals(ROOM_MEMBER_NOT_FOUND.getStatus(), exception.getStatus());
+
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 memberScheduleId에 의한 예외 발생")
+    public void postMSRecord_InvaildMemberSchedule(){
+        Member member = MemberFixture.KIM.생성();
+        MemberSchedule memberSchedule = MSFixture.createMemberScheduleA(RoomFixture.createRoomA());
+        List<LocalDateTime> selectTimes = Arrays.asList(
+                LocalDateTime.of(2023, 1, 1, 9, 30)
+        );
+        MSRecordPostRequest msRecordPostRequest = new MSRecordPostRequest(567L, selectTimes);
+        AuthPrincipal authPrincipal = new AuthPrincipal(1L, member.getNickname().getValue(), member.getRole());
+
+        when(msRepository.findById(msRecordPostRequest.msId())).thenReturn(Optional.empty());
+
+        MSException exception = assertThrows(MSException.class, () -> {
+            msRecordService.postMSRecord(msRecordPostRequest, authPrincipal);
+        });
+
+        assertEquals(SCHEDULE_NOT_FOUND.getMessage(), exception.getMessage());
+        assertEquals(SCHEDULE_NOT_FOUND.getStatus(), exception.getStatus());
+
     }
 
     @Test
@@ -141,6 +209,10 @@ public class MSRecordServiceTest {
         assertFalse("선택 시간 목록이 비어있습니다.", msRecordGetResponse.selectTime().isEmpty());
 
     }
+
+    // 존재하지 않는 룸
+    // 존재하지 않는 일정
+    // 리더 권한이 없는 유저의 get 조회
 
 
 }
