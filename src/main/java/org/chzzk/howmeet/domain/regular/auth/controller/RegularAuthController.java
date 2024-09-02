f2f81edef79ab505fafe5c6515cd3baa33fb64c7
kmw2378 <rlarla677@gmail.com>
2024-09-02 15:20:44 +0900

    [FEAT] RegularAuthController.authorize() 메서드 추가 (#127)

--- src/main/java/org/chzzk/howmeet/domain/regular/auth/controller/RegularAuthController.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/controller/RegularAuthController.java
@@ -2,10 +2,14 @@ package org.chzzk.howmeet.domain.regular.auth.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.domain.regular.auth.dto.authorize.request.MemberAuthorizeRequest;
+import org.chzzk.howmeet.domain.regular.auth.dto.authorize.response.MemberAuthorizeResponse;
 import org.chzzk.howmeet.domain.regular.auth.dto.login.request.MemberLoginRequest;
 import org.chzzk.howmeet.domain.regular.auth.dto.login.response.MemberLoginResponse;
 import org.chzzk.howmeet.domain.regular.auth.service.RegularAuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +20,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegularAuthController {
     private final RegularAuthService regularAuthService;
+
+    @GetMapping("/authorize")
+    public ResponseEntity<?> authorize(@ModelAttribute @Valid final MemberAuthorizeRequest memberAuthorizeRequest) {
+        final MemberAuthorizeResponse memberAuthorizeResponse = regularAuthService.authorize(memberAuthorizeRequest);
+        return ResponseEntity.ok(memberAuthorizeResponse);
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid final MemberLoginRequest memberLoginRequest) {
