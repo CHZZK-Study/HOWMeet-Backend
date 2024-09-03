20987d2f48a421f1cbda5e2ecc380477810e8f02
kmw2378 <rlarla677@gmail.com>
2024-09-03 14:33:11 +0900

    [REFACTOR] RefreshTokenErrorCode 메시지 수정 (#130)

--- src/main/java/org/chzzk/howmeet/domain/regular/auth/exception/RefreshTokenErrorCode.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/auth/exception/RefreshTokenErrorCode.java
@@ -6,9 +6,9 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum RefreshTokenErrorCode implements DomainErrorCode {
-    REFRESH_TOKEN_NO_AUTHORITY("정기 회원이 아닙니다.", HttpStatus.BAD_REQUEST),
+    REFRESH_TOKEN_NO_AUTHORITY("회원이 아닙니다.", HttpStatus.BAD_REQUEST),
     REFRESH_TOKEN_NOT_FOUND("리프레시 토큰을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    REFRESH_TOKEN_NOT_MATCHED("요청 헤더의 리프레시 토큰값에 해당하는 회원이 없습니다.", HttpStatus.NOT_FOUND);
+    REFRESH_TOKEN_NOT_MATCHED("엑세스 토큰의 회원 정보와 리프레시 토큰의 회원 정보가 일치하지 않습니다", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final HttpStatus status;
