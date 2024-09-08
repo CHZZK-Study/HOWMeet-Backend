d3b7ac19536f966f701c3fae444cd414910b9a56
kmw2378 <rlarla677@gmail.com>
2024-09-08 19:42:19 +0900

    [REFACTOR] OAuthAuthorizeRequest 필드 수정 (#137)

--- src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
+++ src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
@@ -4,8 +4,8 @@ import org.chzzk.howmeet.infra.oauth.model.OAuthProvider;
 
 import java.util.List;
 
-public record OAuthAuthorizeRequest(String client_id, String client_secret, List<String> scope) {
+public record OAuthAuthorizeRequest(String client_id, List<String> scope) {
     public static OAuthAuthorizeRequest from(final OAuthProvider oAuthProvider) {
-        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.clientSecret(), oAuthProvider.scope());
+        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.scope());
     }
 }
