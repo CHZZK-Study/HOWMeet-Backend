0f9b8f33b58fa38bf4ad2c0a7ea1304618e01a4f
kmw2378 <rlarla677@gmail.com>
2024-09-08 19:47:46 +0900

    [FEAT] 인가 코드 URL 쿼리 파라미터에 response_type 추가 (#137)

--- src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
+++ src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
@@ -4,8 +4,8 @@ import org.chzzk.howmeet.infra.oauth.model.OAuthProvider;
 
 import java.util.List;
 
-public record OAuthAuthorizeRequest(String client_id, List<String> scope) {
+public record OAuthAuthorizeRequest(String client_id, String response_type, List<String> scope) {
     public static OAuthAuthorizeRequest from(final OAuthProvider oAuthProvider) {
-        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.scope());
+        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.responseType(), oAuthProvider.scope());
     }
 }
