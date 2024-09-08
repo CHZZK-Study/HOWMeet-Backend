7ecffc77330c4f82b9ffd0e1afdeea934fa8403d
kmw2378 <rlarla677@gmail.com>
2024-09-08 19:27:57 +0900

    [FEAT] OAuthAuthorizeRequest 추가 (#137)

---
+++ src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
@@ -0,0 +1,11 @@
+package org.chzzk.howmeet.infra.oauth.dto.authorize.request;
+
+import org.chzzk.howmeet.infra.oauth.model.OAuthProvider;
+
+import java.util.List;
+
+public record OAuthAuthorizeRequest(String client_id, String client_secret, List<String> scope) {
+    public static OAuthAuthorizeRequest from(final OAuthProvider oAuthProvider) {
+        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.clientSecret(), oAuthProvider.scope());
+    }
+}
