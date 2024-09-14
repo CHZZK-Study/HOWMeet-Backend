44341184ef952e2080f9a61f2b9ec24c01f9cd63
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-14 23:23:38 +0900

    [REFACTOR] URL 수정

--- src/main/java/org/chzzk/howmeet/domain/regular/schedule/controller/MSController.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/schedule/controller/MSController.java
@@ -10,25 +10,25 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RequiredArgsConstructor
-@RequestMapping("room")
+@RequestMapping("room/{roomId}")
 @RestController
 public class MSController {
     private final MSService msService;
 
-    @PostMapping("/{roomId}")
+    @PostMapping
     public ResponseEntity<MSResponse> createMemberSchedule(@PathVariable Long roomId, @RequestBody final MSRequest msRequest) {
         final MSResponse msResponse = msService.createMemberSchedule(roomId, msRequest);
         return ResponseEntity.created(URI.create("/member-schedule/" + msResponse.id()))
                 .build();
     }
 
-    @GetMapping("/{roomId}/{msId}")
+    @GetMapping("/{msId}")
     public ResponseEntity<?> getMemberSchedule(@PathVariable Long roomId, @PathVariable Long msId) {
         final MSResponse msResponse = msService.getMemberSchedule(roomId, msId);
         return ResponseEntity.ok(msResponse);
     }
 
-    @DeleteMapping("/{roomId}/{msId}")
+    @DeleteMapping("/{msId}")
     public ResponseEntity<?> deleteGuestSchedule(@PathVariable Long roomId, @PathVariable Long msId) {
         msService.deleteMemberSchedule(roomId, msId);
         return ResponseEntity.ok("Member schedule successfully deleted");
