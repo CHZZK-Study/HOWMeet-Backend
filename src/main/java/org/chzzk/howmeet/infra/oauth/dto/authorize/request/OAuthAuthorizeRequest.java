79ccf8ae530bc4c0168ed95fdc5127f8a51e63da
kmw2378 <rlarla677@gmail.com>
2024-09-18 16:46:30 +0900

    [REFACTOR] 인가 코드 URL 파싱 역할을 다른 클래스로 이전 (#151)

--- src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
+++ src/main/java/org/chzzk/howmeet/infra/oauth/dto/authorize/request/OAuthAuthorizeRequest.java
@@ -2,12 +2,10 @@ package org.chzzk.howmeet.infra.oauth.dto.authorize.request;
 
 import org.chzzk.howmeet.infra.oauth.model.OAuthProvider;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public record OAuthAuthorizeRequest(String client_id, String response_type, List<String> scope, String redirect_uri) {
     public static OAuthAuthorizeRequest from(final OAuthProvider oAuthProvider) {
-        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.responseType(), oAuthProvider.scope(), URLEncoder.encode(oAuthProvider.redirectUrl(), StandardCharsets.UTF_8));
+        return new OAuthAuthorizeRequest(oAuthProvider.clientId(), oAuthProvider.responseType(), oAuthProvider.scope(), oAuthProvider.redirectUrl());
     }
 }
