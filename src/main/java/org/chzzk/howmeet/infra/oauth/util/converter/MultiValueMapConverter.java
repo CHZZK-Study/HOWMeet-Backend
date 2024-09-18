0c6973573fb726ae966523f876f523cee281d598
kmw2378 <rlarla677@gmail.com>
2024-09-18 18:08:55 +0900

    [REFACTOR] MultiValueConverter.convertFrom() 메서드 수정 (#151) 인코딩/디코딩 전략을 파라미터로 전달받아 변환하도록 수정

--- src/main/java/org/chzzk/howmeet/infra/oauth/util/converter/MultiValueMapConverter.java
+++ src/main/java/org/chzzk/howmeet/infra/oauth/util/converter/MultiValueMapConverter.java
@@ -6,32 +6,27 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 @Slf4j
 public final class MultiValueMapConverter {
     public static MultiValueMap<String, String> convertFrom(final ObjectMapper objectMapper,
-                                                            final Object source) {
+                                                            final Object source,
+                                                            final TransformationStrategy strategy) {
         try {
             final Map<String, String> convertedValue = objectMapper.convertValue(source, new TypeReference<>() {
             });
-            return getEncodedParams(convertedValue);
+            final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+            convertedValue.forEach((key, value) -> {
+                final String transformedKey = strategy.transform(key);
+                final String transformedValue = strategy.transform(value);
+                params.add(transformedKey, transformedValue);
+            });
+
+            return params;
         } catch (Exception e) {
             log.error("MultiValueMap으로 변환 실패 : {}", e.getMessage());
             throw new IllegalStateException();
         }
-    }
-
-    private static MultiValueMap<String, String> getEncodedParams(final Map<String, String> convertedValue) {
-        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        convertedValue.forEach((key, value) -> {
-            final String encodedKey = URLEncoder.encode(key, StandardCharsets.UTF_8);
-            final String encodedValue = URLEncoder.encode(value, StandardCharsets.UTF_8);
-            params.add(encodedKey, encodedValue);
-        });
-
-        return params;
     }
 }
