b1b860ba7b02f204c01ac3ff7ed214d915a24cc4
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-19 01:59:25 +0900

    Squashed commit of the following:
    commit d2dce7c28975206caa2f587c933d15bf99b01db6
    Author: Kim Min-Woo <107420002+kmw2378@users.noreply.github.com>
    Date:   Wed Sep 18 19:49:35 2024 +0900
    
        [기능 수정] 소셜 로그인 (#63)

---
+++ src/main/java/org/chzzk/howmeet/infra/oauth/util/mapper/OAuthObjectMapperFactory.java
@@ -0,0 +1,47 @@
+package org.chzzk.howmeet.infra.oauth.util.mapper;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.chzzk.howmeet.infra.oauth.util.serilaizer.ListToCommaSeparatedStringSerializer;
+import org.chzzk.howmeet.infra.oauth.util.serilaizer.ListToSpaceSeparatedStringSerializer;
+
+import java.nio.file.ProviderNotFoundException;
+import java.util.List;
+import java.util.Map;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class OAuthObjectMapperFactory {
+    private static final Map<String, ObjectMapper> MAPPER_MAP;
+    private static final ObjectMapper LIST_TO_COMMA_MAPPER;
+    private static final ObjectMapper LIST_TO_SPACE_MAPPER;
+
+    static {
+        LIST_TO_COMMA_MAPPER = JsonMapper.builder()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .addModule(new SimpleModule().addSerializer(List.class, new ListToCommaSeparatedStringSerializer()))
+                .build();
+
+        LIST_TO_SPACE_MAPPER = JsonMapper.builder()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .addModule(new SimpleModule().addSerializer(List.class, new ListToSpaceSeparatedStringSerializer()))
+                .build();
+
+        MAPPER_MAP = Map.of(
+                "kakao", LIST_TO_COMMA_MAPPER,
+                "naver", LIST_TO_COMMA_MAPPER,
+                "google", LIST_TO_SPACE_MAPPER
+        );
+    }
+
+    public static ObjectMapper getFrom(final String providerName) {
+        if (!MAPPER_MAP.containsKey(providerName)) {
+            throw new ProviderNotFoundException(providerName);
+        }
+
+        return MAPPER_MAP.get(providerName);
+    }
+}
