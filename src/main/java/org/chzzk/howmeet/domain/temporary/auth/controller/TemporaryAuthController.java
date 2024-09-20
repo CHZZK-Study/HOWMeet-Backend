7fbe6d958877008a87c51ac12fc3e07f0a417d3b
kmw2378 <rlarla677@gmail.com>
2024-09-20 13:41:09 +0900

    [REFACTOR] 기존 Guest 관련 Controller, Test 클래스명 수정 (#153)

--- src/main/java/org/chzzk/howmeet/domain/temporary/auth/controller/TemporaryAuthController.java
+++ src/main/java/org/chzzk/howmeet/domain/temporary/auth/controller/TemporaryAuthController.java
@@ -4,7 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.chzzk.howmeet.domain.temporary.auth.dto.login.request.GuestLoginRequest;
 import org.chzzk.howmeet.domain.temporary.auth.dto.login.response.GuestLoginResponse;
-import org.chzzk.howmeet.domain.temporary.auth.service.GuestService;
+import org.chzzk.howmeet.domain.temporary.auth.service.TemporaryAuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,12 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 @RestController
-public class GuestController {
-    private final GuestService guestService;
+public class TemporaryAuthController {
+    private final TemporaryAuthService temporaryAuthService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid final GuestLoginRequest guestLoginRequest) {
-        final GuestLoginResponse guestLoginResponse = guestService.login(guestLoginRequest);
+        final GuestLoginResponse guestLoginResponse = temporaryAuthService.login(guestLoginRequest);
         return ResponseEntity.ok(guestLoginResponse);
     }
 }
