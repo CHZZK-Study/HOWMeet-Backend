801ca427cae8a944fc9726d15b219c7fc45b1619
chech2 <chaelim3396@gmail.com>
2024-09-23 18:44:28 +0900

    [REFACTOR] ConfirmScheduleResponse 변경 (#116) - 요청사항에 맞게 변수명 변경

--- src/main/java/org/chzzk/howmeet/domain/regular/confirm/dto/ConfirmScheduleResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/confirm/dto/ConfirmScheduleResponse.java
@@ -7,16 +7,16 @@ import org.chzzk.howmeet.domain.common.model.SelectionDetail;
 import org.chzzk.howmeet.domain.regular.confirm.entity.ConfirmSchedule;
 import org.chzzk.howmeet.domain.regular.room.model.RoomName;
 
-public record ConfirmScheduleResponse(RoomName roomName, String msName, List<LocalDateTime> time, List<String> participantMembers, Nicknames totalRoomMembers, List<SelectionDetail> allSelectTime) {
+public record ConfirmScheduleResponse(RoomName roomName, String msName, List<LocalDateTime> confirmTime, List<String> participantPerson, Nicknames totalPersonnel, List<SelectionDetail> time) {
 
-    public static ConfirmScheduleResponse of(final ConfirmSchedule confirmSchedule, final Nicknames totalRoomMembers, final List<SelectionDetail> allSelectTime) {
+    public static ConfirmScheduleResponse of(final ConfirmSchedule confirmSchedule, final Nicknames totalPersonnel, final List<SelectionDetail> time) {
         return new ConfirmScheduleResponse(
                 confirmSchedule.getMs().getRoom().getName(),
                 confirmSchedule.getMs().getName().getValue(),
                 confirmSchedule.getTimes(),
                 confirmSchedule.getParticipantName(),
-                totalRoomMembers,
-                allSelectTime
+                totalPersonnel,
+                time
         );
     }
 }
