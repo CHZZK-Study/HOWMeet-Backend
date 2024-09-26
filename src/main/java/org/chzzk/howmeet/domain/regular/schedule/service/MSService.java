33c294ceb366131cc4da0fbb995931e22db23050
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-27 02:30:36 +0900

    [기능 수정]  회원 일정 조회 출력 변경, 비회원 일정 생성시 응답값 수정 (#78)
    * [REFACTOR] 일정 조회시 status에 따라 다르게 조회 (#156)
    
    * [REFACTOR] 비회원 생성시 gsId 반환 (#156)

--- src/main/java/org/chzzk/howmeet/domain/regular/schedule/service/MSService.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/schedule/service/MSService.java
@@ -1,15 +1,27 @@
 package org.chzzk.howmeet.domain.regular.schedule.service;
 
 import lombok.RequiredArgsConstructor;
+import org.chzzk.howmeet.domain.common.embedded.date.impl.ScheduleTime;
+import org.chzzk.howmeet.domain.regular.confirm.entity.ConfirmSchedule;
+import org.chzzk.howmeet.domain.regular.confirm.repository.ConfirmRepository;
 import org.chzzk.howmeet.domain.regular.room.entity.Room;
 import org.chzzk.howmeet.domain.regular.room.repository.RoomRepository;
+import org.chzzk.howmeet.domain.regular.schedule.dto.CompletedMSResponse;
 import org.chzzk.howmeet.domain.regular.schedule.dto.MSRequest;
 import org.chzzk.howmeet.domain.regular.schedule.dto.MSResponse;
+import org.chzzk.howmeet.domain.regular.schedule.dto.ProgressedMSResponse;
 import org.chzzk.howmeet.domain.regular.schedule.entity.MemberSchedule;
+import org.chzzk.howmeet.domain.regular.schedule.entity.ScheduleStatus;
 import org.chzzk.howmeet.domain.regular.schedule.exception.MSException;
 import org.chzzk.howmeet.domain.regular.schedule.repository.MSRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.chzzk.howmeet.domain.regular.schedule.exception.MSErrorCode.ROOM_NOT_FOUND;
 import static org.chzzk.howmeet.domain.regular.schedule.exception.MSErrorCode.SCHEDULE_NOT_FOUND;
@@ -20,21 +32,33 @@ import static org.chzzk.howmeet.domain.regular.schedule.exception.MSErrorCode.SC
 public class MSService {
     private final MSRepository msRepository;
     private final RoomRepository roomRepository;
+    private final ConfirmRepository confirmRepository;
 
     @Transactional
-    public MSResponse createMemberSchedule(final Long roomId, final MSRequest msRequest) {
+    public ProgressedMSResponse createMemberSchedule(final Long roomId, final MSRequest msRequest) {
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new MSException(ROOM_NOT_FOUND));
 
         MemberSchedule memberSchedule = msRequest.toEntity(room);
         MemberSchedule savedSchedule = msRepository.save(memberSchedule);
 
-        return MSResponse.from(savedSchedule);
+        return ProgressedMSResponse.from(savedSchedule);
     }
 
     public MSResponse getMemberSchedule(final Long roomId, final Long msId) {
         MemberSchedule memberSchedule = findMemberScheduleByRoomIdAndMsId(roomId, msId);
-        return MSResponse.from(memberSchedule);
+
+        if (memberSchedule.getStatus() == ScheduleStatus.COMPLETE) {
+            ConfirmSchedule confirmSchedule = findConfirmScheduleByMSId(msId);
+            List<LocalDateTime> confirmTimes = confirmSchedule.getTimes();
+            List<String> dates = extractDatesFromConfirmTimes(confirmTimes);
+            ScheduleTime scheduleTime = extractScheduleTimeFromConfirmTimes(confirmTimes);
+            return CompletedMSResponse.of(memberSchedule, dates, scheduleTime);
+        } else if (memberSchedule.getStatus() == ScheduleStatus.PROGRESS) {
+            return ProgressedMSResponse.from(memberSchedule);
+        }
+
+        throw new IllegalStateException("알 수 없는 상태입니다.");
     }
 
     @Transactional
@@ -47,6 +71,22 @@ public class MSService {
     private MemberSchedule findMemberScheduleByRoomIdAndMsId(Long roomId, Long msId) {
         return msRepository.findByIdAndRoomId(msId, roomId)
                 .orElseThrow(() -> new MSException(SCHEDULE_NOT_FOUND));
+    }
+
+    private ConfirmSchedule findConfirmScheduleByMSId(Long memberScheduleId) {
+        return confirmRepository.findByMemberScheduleId(memberScheduleId)
+                .orElseThrow(() -> new IllegalArgumentException("일치하는 일정 결과를 찾을 수 없습니다."));
+    }
+
+    private List<String> extractDatesFromConfirmTimes(List<LocalDateTime> confirmTimes) {
+        String date = confirmTimes.get(0).toLocalDate().toString();
+        return List.of(date);
+    }
+
+    private ScheduleTime extractScheduleTimeFromConfirmTimes(List<LocalDateTime> confirmTimes) {
+        LocalTime startTime = confirmTimes.get(0).toLocalTime();
+        LocalTime endTime = confirmTimes.get(1).toLocalTime();
+        return ScheduleTime.of(startTime, endTime);
     }
 }
 
