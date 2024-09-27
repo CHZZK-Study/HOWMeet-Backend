13822d8dfeac4bbec6eaab976a07b0e68c5f3394
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-27 16:38:47 +0900

    [REFACTOR] CompletedMSResponse 수정(#159)

--- src/main/java/org/chzzk/howmeet/domain/regular/schedule/dto/CompletedMSResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/schedule/dto/CompletedMSResponse.java
@@ -8,12 +8,14 @@ import org.chzzk.howmeet.domain.regular.schedule.entity.ScheduleStatus;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record CompletedMSResponse(Long id, List<String> dates, ScheduleTime time, ScheduleName name, ScheduleStatus status) implements MSResponse {
-    public static CompletedMSResponse of(final MemberSchedule memberSchedule, List<String> dates, ScheduleTime time) {
+public record CompletedMSResponse(Long id, List<String> dates, ScheduleTime time, List<String> confirmedDates, ScheduleTime confirmedTime,ScheduleName name, ScheduleStatus status) implements MSResponse {
+    public static CompletedMSResponse of(final MemberSchedule memberSchedule, List<String> confirmedDates, ScheduleTime confirmedTime) {
         return new CompletedMSResponse(
                 memberSchedule.getId(),
-                dates,
-                time,
+                memberSchedule.getDates(),
+                memberSchedule.getTime(),
+                confirmedDates,
+                confirmedTime,
                 memberSchedule.getName(),
                 memberSchedule.getStatus()
         );
