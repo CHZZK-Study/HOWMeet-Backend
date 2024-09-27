041534a23c2e9305ae2b7d71b4a23be2b9b826b9
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-28 00:48:47 +0900

    [REFACTOR] 회원 일정 생성시 응답값 추가(#159)

--- src/main/java/org/chzzk/howmeet/domain/regular/schedule/controller/MSController.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/schedule/controller/MSController.java
@@ -2,6 +2,7 @@ package org.chzzk.howmeet.domain.regular.schedule.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.chzzk.howmeet.domain.regular.auth.annotation.RegularUser;
+import org.chzzk.howmeet.domain.regular.schedule.dto.MSCreateResponse;
 import org.chzzk.howmeet.domain.regular.schedule.dto.MSRequest;
 import org.chzzk.howmeet.domain.regular.schedule.dto.MSResponse;
 import org.chzzk.howmeet.domain.regular.schedule.dto.ProgressedMSResponse;
@@ -19,14 +20,12 @@ public class MSController {
 
     @PostMapping
     @RegularUser
-    public ResponseEntity<ProgressedMSResponse> createMemberSchedule(@PathVariable Long roomId, @RequestBody final MSRequest msRequest) {
-        final ProgressedMSResponse progressedMsResponse = msService.createMemberSchedule(roomId, msRequest);
-        return ResponseEntity.created(URI.create("/member-schedule/" + progressedMsResponse.id()))
-                .build();
+    public ResponseEntity<?> createMemberSchedule(@PathVariable Long roomId, @RequestBody final MSRequest msRequest) {
+        final MSCreateResponse msCreateResponse = msService.createMemberSchedule(roomId, msRequest);
+        return ResponseEntity.ok(msCreateResponse);
     }
 
     @GetMapping("/{msId}")
-    @RegularUser
     public ResponseEntity<?> getMemberSchedule(@PathVariable(name = "roomId") Long roomId, @PathVariable(name = "msId") Long msId) {
         final MSResponse msResponse = msService.getMemberSchedule(roomId, msId);
         return ResponseEntity.ok(msResponse);
