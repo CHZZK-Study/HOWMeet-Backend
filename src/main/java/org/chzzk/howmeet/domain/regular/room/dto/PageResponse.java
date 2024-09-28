18b415edb4328a3c4a698385124859ac73223a0f
SSUHYUNKIM <yellosuhyun@naver.com>
2024-09-28 17:21:56 +0900

    [머지 브랜치] 4.8차 배포 (#89)
    * [기능 수정] 자정 지남 여부 추가 (#74)
    
    * [REFACTOR] 자정 지남 여부 추가 (#157)
    
    * [REFACTOR] 불필요한 메서드 삭제 (#157)
    
    * [코드 정리] @TemporaryUser, @regular 추가 (#158) (#73)
    
    * [REFACTOR] @TemporaryUser, @regular 추가 (#158)
    
    * [REFACTOR] @regular 추가 (#158)
    
    * [REFACTOR] 자정 시간 로직 추가 (#158)
    
    * [HOTFIX] 빌드 에러 수정
    
    * [기능 수정]  회원 일정 조회 출력 변경, 비회원 일정 생성시 응답값 수정 (#78)
    
    * [REFACTOR] 일정 조회시 status에 따라 다르게 조회 (#156)
    
    * [REFACTOR] 비회원 생성시 gsId 반환 (#156)
    
    * Revert "[기능 수정]  회원 일정 조회 출력 변경, 비회원 일정 생성시 응답값 수정 (#78)" (#79)
    
    This reverts commit 33c294ceb366131cc4da0fbb995931e22db23050.
    
    * [기능 수정] 누락된 부분 반영 (#80)
    
    * [REFACTOR] 일정 조회시 status에 따라 다르게 조회 (#156)
    
    * [REFACTOR] 비회원 생성시 gsId 반환 (#156)
    
    * [REFACTOR] 회원 일정 응답값 수정 (#156)
    
    * [기능 수정] 해당 멤버가 일정에 참여했는지 확인하는 로직 추가 (#82)
    
    * [REFACTOR] CompletedMSResponse 수정(#159)
    
    * [REFACTOR] MSRecordRepository 수정(#159)
    
    * [REFACTOR] 해당 멤버가 일정에 참여했는지 확인하는 로직 추가(#159)
    
    * [REFACTOR] 구분 로직 추가(#159)
    
    * [기능 수정] 회원 일정 생성시 id값 반환 (#84)
    
    * [REFACTOR] CompletedMSResponse 수정(#159)
    
    * [REFACTOR] MSRecordRepository 수정(#159)
    
    * [REFACTOR] 해당 멤버가 일정에 참여했는지 확인하는 로직 추가(#159)
    
    * [REFACTOR] 구분 로직 추가(#159)
    
    * [REFACTOR] 회원 일정 생성시 응답값 추가(#159)
    
    * [기능 수정] 회원 일정 생성시 룸 id값 반환 (#86)
    
    * [REFACTOR] CompletedMSResponse 수정(#159)
    
    * [REFACTOR] MSRecordRepository 수정(#159)
    
    * [REFACTOR] 해당 멤버가 일정에 참여했는지 확인하는 로직 추가(#159)
    
    * [REFACTOR] 구분 로직 추가(#159)
    
    * [REFACTOR] 회원 일정 생성시 응답값 추가(#159)
    
    * [REFACTOR] 회원 일정 생성시 응답값 추가(#159)
    
    * [REFACTOR] 참여한 방의 갯수 응답값 추가(#162) (#88)
    
    ---------
    
    Co-authored-by: Leechaelim <chaelim3396@gmail.com>

--- src/main/java/org/chzzk/howmeet/domain/regular/room/dto/PageResponse.java
+++ src/main/java/org/chzzk/howmeet/domain/regular/room/dto/PageResponse.java
@@ -3,13 +3,14 @@ package org.chzzk.howmeet.domain.regular.room.dto;
 import java.util.List;
 
 public record PageResponse(
+        int totalRoomCount,
         List<?> roomList,
         int currentPage,
         int totalPages,
         boolean hasNextPage
 ) {
 
-    public static PageResponse of(List<?> romList, int currentPage, int totalPages, boolean hasNextPage) {
-        return new PageResponse(romList, currentPage, totalPages, hasNextPage);
+    public static PageResponse of(int totalRoomCount,List<?> romList, int currentPage, int totalPages, boolean hasNextPage) {
+        return new PageResponse(totalRoomCount, romList, currentPage, totalPages, hasNextPage);
     }
 }
